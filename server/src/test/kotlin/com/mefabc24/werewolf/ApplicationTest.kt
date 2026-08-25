047d020382de68f19b3d74ddd3f9e8456b20b05c
mefabc24 <fabian.mecke@student.reutlingen-university.de>
2026-08-26 01:39:07 +0200

    remove unknown function call

--- server/src/test/kotlin/com/mefabc24/werewolf/ApplicationTest.kt
+++ server/src/test/kotlin/com/mefabc24/werewolf/ApplicationTest.kt
@@ -11,7 +11,7 @@ class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
         application {
-            module()
+
         }
         val response = client.get("/")
         assertEquals(HttpStatusCode.OK, response.status)
