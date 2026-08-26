6c2a7c569934fa1b8aa58937961949ead129fb15
mefabc24 <fabian.mecke@student.reutlingen-university.de>
2026-08-26 12:49:02 +0200

    feat: enhance GameController to manage game state and player information

--- app/shared/src/commonMain/kotlin/com/mefabc24/werewolf/game/GameController.kt
+++
@@ -1,5 +0,0 @@
-package com.mefabc24.werewolf.game
-
-class GameController {
-    fun start() {}
-}
---
+++ server/src/main/kotlin/com/mefabc24/werewolf/game/GameController.kt
@@ -0,0 +1,37 @@
+package com.mefabc24.werewolf.game
+
+import com.mefabc24.werewolf.player.Player
+import com.mefabc24.werewolf.player.PlayerInfo
+import com.mefabc24.werewolf.player.role.Team
+
+class GameController(
+    private val gameState: GameState
+) {
+
+    fun start(players: List<Player>) {
+        gameState.players.clear()
+        gameState.players.addAll(players)
+
+        gameState.round = 1
+        gameState.gamePhase = GamePhase.START
+    }
+
+    fun getClientGameState(playerId: Int): ClientGameState {
+        val player = gameState.players.find { it.id == playerId }
+
+        val werewolfIds =
+            if (player?.role?.team == Team.WEREWOLVES) {
+                gameState.players
+                    .filter { it.role?.team == Team.WEREWOLVES }
+                    .map { it.id }
+            } else null
+
+        return ClientGameState(
+            players = gameState.players.map { PlayerInfo(it.id, it.name) },
+            gamePhase = gameState.gamePhase,
+            nightPhase = gameState.nightPhase,
+            round = gameState.round,
+            werewolfIds = werewolfIds
+        )
+    }
+}
