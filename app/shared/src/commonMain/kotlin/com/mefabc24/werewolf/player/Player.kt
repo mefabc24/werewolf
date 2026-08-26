96d768a8ff54cd35f7cbff2265a8abc3c1848fa0
mefabc24 <fabian.mecke@student.reutlingen-university.de>
2026-08-26 12:47:11 +0200

    feat: add role property to Player data class

--- app/shared/src/commonMain/kotlin/com/mefabc24/werewolf/player/Player.kt
+++ app/shared/src/commonMain/kotlin/com/mefabc24/werewolf/player/Player.kt
@@ -1,6 +1,9 @@
 package com.mefabc24.werewolf.player
 
+import com.mefabc24.werewolf.player.role.Role
+
 data class Player(
     val id: Int,
-    val name: String
+    val name: String,
+    var role: Role? = null
 )
